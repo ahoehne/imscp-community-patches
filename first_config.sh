83754ea8f56245c65635668168881fef19a6e010
Webdesign Höhne <info@webdesign-hoehne.de>
2013-04-03 13:07:09 +0200

    check if script is correctly installed (correct base of /root/imscp_patch/.git)

--- first_config.sh
+++ first_config.sh
@@ -3,22 +3,26 @@
 
 # Requirement: preinstalled community-patcher in /root/imscp_patch ; logged in as root #
 cd /root/imscp_patch/
-git pull
-#==== Install/ Patching of Debian Packages ====
-aptitude update && aptitude safe-upgrade
-aptitude install -y git lsb-release
-#==== Get actual State of git master ====
-echo "#### KLONE i-MSCP git/master branch ####"
-mkdir -p /usr/local/src/imscp
-if [ ! -d /usr/local/src/imscp/.git ]; then
-	git clone git://github.com/i-MSCP/imscp.git /usr/local/src/imscp
-else
-	cd /usr/local/src/imscp
+if [ ! -d /root/imscp_patch/.git ]; then
 	git pull
+	#==== Install/ Patching of Debian Packages ====
+	aptitude update && aptitude safe-upgrade
+	aptitude install -y git lsb-release
+	#==== Get actual State of git master ====
+	echo "#### KLONE i-MSCP git/master branch ####"
+	mkdir -p /usr/local/src/imscp
+	if [ ! -d /usr/local/src/imscp/.git ]; then
+		git clone git://github.com/i-MSCP/imscp.git /usr/local/src/imscp
+	else
+		cd /usr/local/src/imscp
+		git pull
+	fi
+	
+	# Todo: Modularer Aufbau für Community Patches
+	
+	
+	
+	#perl /usr/local/src/imscp/imscp-autoinstall 
+else
+	echo 'Cannot find git informations for /root/imscp_patch/ , this script is not installed properly'
 fi
-
-# Todo: Modularer Aufbau für Community Patches
-
-
-
-#perl /usr/local/src/imscp/imscp-autoinstall 
