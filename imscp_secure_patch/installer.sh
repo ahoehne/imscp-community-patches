c1ce6097df60206613a34e7e7a4b7b2039c3feb9
Webdesign Höhne <info@webdesign-hoehne.de>
2013-04-03 13:45:24 +0200

    secure_patch installer

--- imscp_secure_patch/installer.sh
+++ imscp_secure_patch/installer.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 chmod 700 ~/imscp_patch/imscp_secure_patch/patch.sh
 aptitude update && aptitude safe-upgrade
-aptitude install -y htpasswd
+aptitude install -y apache2-utils
 ### You may change the name "admin" to whatever you want ###
 htpasswd -c /root/imscp_secure_patch/.htpasswd admin
