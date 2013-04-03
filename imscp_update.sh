fd0a8ae2aa62d540d3564982930cff2a68dd9a7a
Webdesign Höhne <info@webdesign-hoehne.de>
2013-04-03 14:01:25 +0200

    looks nicer

--- imscp_update.sh
+++ imscp_update.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
-cd /root/imscp_patch/
 if [ ! -d /root/imscp_patch/.git ]; then
+	cd /root/imscp_patch/
+	echo "#### Get actual Community Patcher ####"
 	git pull
 	mkdir -p /usr/local/src/imscp
 	if [ ! -d /usr/local/src/imscp/.git ]; then
+		echo "#### CLONING i-MSCP git/master branch ####"
 		cd /usr/local/src/imscp
 		git pull
 		# Todo: Check ob installer ausgeführt wurde (Modulare Config)
-		# Pre Patching
+		echo "#### pre-patching i-mscp ####"
 		cd /root/imscp_patch/
 		/root/imscp_patch/pre_patch.sh
-		# start imscp installer
+		echo "#### starting i-mscp installer ####"
 		cd /usr/local/src/imscp/
 		perl /usr/local/src/imscp/imscp-autoinstall 
-		# Post Patching
+		echo "#### post-patching i-mscp ####"
 		cd /root/imscp_patch/
 		/root/imscp_patch/post_patch.sh
 	else
